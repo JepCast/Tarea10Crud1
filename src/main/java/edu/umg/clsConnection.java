93e0bc769aa5c9318b731100f97cf9a30d237b4b
JepCast <ycastillop2@miumg.edu.gt>
2023-09-29 20:11:11 -0600

    Agregue la lista de datos, tanto al eliminar como actualizar propiedades

--- src/main/java/edu/umg/clsConnection.java
+++ src/main/java/edu/umg/clsConnection.java
@@ -14,55 +14,6 @@ public class clsConnection {
         }
     }
 
-//    public  void readData() throws SQLException {
-//        connect();
-//        String query = "SELECT * FROM tb_personas";
-//        PreparedStatement ps = null;
-//        ResultSet rs = null;
-//
-//        try {
-//            ps = conn.prepareStatement(query);
-//            rs = ps.executeQuery();
-//
-//            while(rs.next()) {
-//                System.out.println("Nombre: " + rs.getString("nombre") + " Apellido: " + rs.getString("apellido"));
-//            }
-//        } finally {
-//            if (rs == null) {
-//                rs.close();
-//            }
-//
-//            if (ps == null) {
-//                ps.close();
-//            }
-//            conn.close();
-//        }
-//    }
-//
-//    public void insertData() throws SQLException {
-//        connect();
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Ingrese el nombre: ");
-//        String nombre = sc.nextLine();
-//        System.out.println("Ingrese el apellido: ");
-//        String apellido = sc.nextLine();
-//
-//        String query = "INSERT INTO tb_personas (nombre, apellido) VALUES (?, ?)";
-//        PreparedStatement ps = null;
-//
-//        try {
-//            ps = conn.prepareStatement(query);
-//            ps.setString(1, nombre);
-//            ps.setString(2, apellido);
-//            ps.executeUpdate();
-//            System.out.println("Datos insertados correctamente");
-//        } finally {
-//            if (ps == null) {
-//                ps.close();
-//            }
-//            conn.close();
-//        }
-//    }
     public void closeConnection() {
         try {
             if (conn != null) {
